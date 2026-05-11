48c5a7404c60ecd6be768b94b6c5566677df0356
MICHELLE <michelleliman286@gmail.com>
2026-05-11 11:46:57 +0700

    Pertemuan 11

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiDBController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -50,3 +51,5 @@ Route::get('/tugaspertemuan5', function () {
 Route::get('/menu', function () {
     return view('menu');
 });
+
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
